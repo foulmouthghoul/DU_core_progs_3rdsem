49650ac40cdbb04274c1ea2860e3a51f64b33e1a
Pritam Chakraborty <72512320+foulmouthghoul@users.noreply.github.com>
2021-06-19 12:38:29 +0530

    Add files via upload

---
+++ finalc.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+/*float x, t, s;
+
+int n;
+
+printf("supply x and the number of termsn \n");
+scanf("%f,%d", &x, &n);
+
+s=0.0; t=1.0;
+
+for (int i=1; i<n; i++)
+{
+    t *= -x / i;
+    s += t;
+
+}
+
+printf("\n");
+
+printf("sum = %.5e", s);
+*/
+
+/*int a[5] = {5, 1, 15, 20, 25};
+
+int i, j, m;
+
+i = ++a[1];
+
+j = a[1]++;
+
+m = a[i++];
+
+printf("%d,%d, %d", i, j, m);
+*/
+int i;
+ for(i=1;i<9;)
+  printf("%d",i);
+   i=i+2;
+return 0;
+}
